2574e83ad6743f0b339d2b046a00845cdf283557
minhtranx <minhtran.tech@outlook.com>
2020-12-25 22:39:07 +0700

    Repo Init

---
+++ server/src/auth/dto/auth-credentials.dto.ts
@@ -0,0 +1,16 @@
+import { IsString, MinLength, MaxLength, Matches } from 'class-validator';
+
+export class AuthCredentialsDto {
+  @IsString()
+  @MinLength(4)
+  @MaxLength(20)
+  username: string;
+
+  @IsString()
+  @MinLength(8)
+  @MaxLength(20)
+  @Matches(/(?:(?=.*\d)|(?=.*\W+))(?![.\n])(?=.*[A-Z])(?=.*[a-z]).*$/, {
+    message: `Password must contain at least 1 uppercase, 1 lowercase and 1 number or special character`,
+  })
+  password: string;
+}
